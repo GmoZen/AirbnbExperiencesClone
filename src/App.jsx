1dabecd6dadcfb7bcef1336af7e9f8b5e26b9de7
GmoZen <zendejas.guillermo@gmail.com>
2023-09-20 01:47:23 -0700

    Move images to public folder and add use of props to Card component

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,15 @@ function App() {
       <div>
         <NavBar />
         <Hero />
-        <Card />
+        <Card 
+          img="katie-zaferes.png"
+          rating="5.0"
+          reviewCount={6}
+          country="USA"
+          title="Life Lessons with Katie Zaferes"
+          price={136}
+
+        />
       </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,20 @@
 
 import React from 'react'
-import image from '../assets/katie-zaferes.png'
 import starImage from '../assets/Star1.png'
 
 
 function Card(props) {
     return (
         <div className='card'>
-            <img src={image} className='card--image'/>
+            <img src={`../../public/images/${props.img}`} className='card--image'/>
             <div className="card--stats">
                 <img src={starImage} className='card--star' />
-                <span>5.0</span>
-                <span className='gray'>(6) • </span>
-                <span className='gray'>USA</span>
+                <span>{props.rating}</span>
+                <span className='gray'>({props.reviewCount}) • </span>
+                <span className='gray'>{props.country}</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className='bold'>From $136</span> / person</p>
+            <p>{props.title}</p>
+            <p><span className='bold'>From ${props.price}</span> / person</p>
         </div>
     )
 }
