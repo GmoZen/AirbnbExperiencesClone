6b9a2d4e5ae78be8e70c82c32e6de9d5d54b62e2
GmoZen <zendejas.guillermo@gmail.com>
2023-09-27 04:17:03 -0700

    Map data into cards, and add them to App's return object

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,35 @@ import viteLogo from '/vite.svg'
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
 import Card from './components/Card'
+import data from './data.js'
+
+
 
 function App() {
+
+  const cards = data.map(item => {
+    return (
+      <Card 
+          img={item.coverImg}
+          rating={item.stats.rating}
+          reviewCount={item.stats.reviewCount}
+          country={item.location}
+          title={item.title}
+          price={item.price}
+
+    />
+    )
+    
+
+  })
   
 
   return (
       <div>
         <NavBar />
         <Hero />
-        <Card 
-          img="katie-zaferes.png"
-          rating="5.0"
-          reviewCount={6}
-          country="USA"
-          title="Life Lessons with Katie Zaferes"
-          price={136}
-
-        />
+        {cards}
+        
       </div>
   )
 }
